1013dff9e0ec114d2475bddc88dbd5bb13d44246
Tonydavidx <49521558+Tonydavidx@users.noreply.github.com>
2019-09-04 13:20:51 +0530

    Delete 99 Bottles of Beer.py

--- 99 Bottles of Beer.py
+++
@@ -1,5 +0,0 @@
-botlles = 99 
-while botlles > 1:
-    print(f"{bottles} bottles of beer on the wall, 99 bottles of beer.")
-    bot= bottles -1
-    print(f"Take one down and pass it around, {bottles} bottles of beer on the wall.")
