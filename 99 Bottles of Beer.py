4588fb0670c7eb48fd598b12e7c21654e87a88b5
Tonydavidx <49521558+Tonydavidx@users.noreply.github.com>
2019-09-04 13:18:54 +0530

    Add files via upload

---
+++ 99 Bottles of Beer.py
@@ -0,0 +1,5 @@
+botlles = 99 
+while botlles > 1:
+    print(f"{bottles} bottles of beer on the wall, 99 bottles of beer.")
+    bot= bottles -1
+    print(f"Take one down and pass it around, {bottles} bottles of beer on the wall.")
